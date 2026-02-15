1abbc4a3cdd4d4e58c0d95e79b6023d4d991f2bc
chocoboy08 <chocoboy0508@gmail.com>
2026-02-16 08:37:35 +0900

    feat: UserInfo 수정

--- src/components/myPage/UserInfo.tsx
+++ src/components/myPage/UserInfo.tsx
@@ -1,36 +1,46 @@
+import { GitHubIcon } from '@/assets/GitHubIcon';
 import { Flex, Text } from '@/components/common/Wrapper';
-import { useLogout } from '@/hooks/mutation';
+import { useGetAccountInfo } from '@/hooks/query';
+import useAccountInfoStore from '@/hooks/zustand/useAccountInfo';
 import { User } from '@/types/user';
-import { color, typography } from 'wowds-tokens';
+import { useEffect } from 'react';
+import { color } from 'wowds-tokens';
 
 const UserInfo = ({ member }: { member: User }) => {
-  const { mutate } = useLogout();
-  const handleLogoutClick = () => {
-    mutate();
-  };
-
+  useGetAccountInfo(); // API 호출 및 zustand에 저장
+  const githubHandle = useAccountInfoStore((state) => state.githubHandle);
+  useEffect(() => {
+    console.log(githubHandle);
+  }, [githubHandle]);
   return (
-    <Flex gap="sm" direction="column">
-      {member.info.name ? (
-        <p style={typography.h1} color={color.textBlack}>
-          {member.info.name} 님
-        </p>
-      ) : (
-        <Text typo="h1" color="darkDisabled">
-          정보를 입력해주세요
-        </Text>
-      )}
-
-      <div
-        onClick={handleLogoutClick}
-        style={{
-          textDecoration: 'underline',
-          cursor: 'pointer',
-          ...typography.label1,
-          color: color.sub
-        }}>
-        로그아웃
-      </div>
+    <Flex gap="sm" direction="column" align="flex-start">
+      <Flex gap="sm" direction="row" align="center" justify="flex-start">
+        {member.info.name ? (
+          <Text typo="h1" color={'textBlack'}>
+            {member.info.name} 님
+          </Text>
+        ) : (
+          <Text typo="h1" color="darkDisabled">
+            정보를 입력해주세요
+          </Text>
+        )}
+        {githubHandle && (
+          <Flex
+            style={{
+              width: 'fit-content',
+              padding: '8px 12px',
+              borderRadius: 40,
+              background: '#fff',
+              borderWidth: 1,
+              borderStyle: 'solid',
+              borderColor: color.outline
+            }}
+            gap="xxs">
+            <GitHubIcon width={14} height={14} />
+            <Text typo="label2">{githubHandle}</Text>
+          </Flex>
+        )}
+      </Flex>
     </Flex>
   );
 };
