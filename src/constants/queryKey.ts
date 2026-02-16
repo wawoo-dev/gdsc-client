c69058d332ff2354d114bad94a105a9dd3347b8a
chocoboy08 <chocoboy0508@gmail.com>
2026-02-16 11:37:55 +0900

    feat: 학번 중복 확인, 과거 가입 정보 조회 API 추가

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -3,7 +3,9 @@ const enum QueryKeys {
   StudentEmailVerification = 'STUDENT_EMAIL_VERIFICATION',
   StudentVerification = 'STUDENT_VERIFICATION',
   DiscordJoined = 'DISCORD_JOINED',
-  AccountInfo = 'ACCOUNT_INFO'
+  AccountInfo = 'ACCOUNT_INFO',
+  CheckStudentId = 'CHECK_STUDENT_ID',
+  PreviousMemberInfo = 'PREVIOUS_MEMBER_INFO'
 }
 
 export default QueryKeys;
