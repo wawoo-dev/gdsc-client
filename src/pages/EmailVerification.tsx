33c54839d9198df05f9a570b41d0a58ecde6ccc4
chocoboy08 <chocoboy0508@gmail.com>
2026-03-05 15:38:52 +0900

    feat: 회원 정보 등록 페이지 데스크탑 반응형 추가

--- src/pages/EmailVerification.tsx
+++ src/pages/EmailVerification.tsx
@@ -1,11 +1,17 @@
-import { Flex, GuideList, Text } from '@/components/common/Wrapper';
+import {
+  Flex,
+  GuideList,
+  MobileBreak,
+  Text
+} from '@/components/common/Wrapper';
 import useSendVerifyEmail from '@/hooks/mutation/useSendVerifyEmail';
 import { media } from '@/styles';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { color } from 'wowds-tokens';
+import { color, typography } from 'wowds-tokens';
 import Box from 'wowds-ui/Box';
 import Button from 'wowds-ui/Button';
 
@@ -60,54 +66,108 @@ export const EmailVerification = () => {
 
   return (
     <Container direction="column" align="flex-start" justify="space-between">
-      <Flex gap="xl" direction="column">
-        <Flex direction="column" align="flex-start" gap="sm">
-          <Text typo="h1">본인 인증하기</Text>
-          <Text typo="body1">
-            기존에 입력하신 본인 이메일을 통해 현재 계정의 <br />
-            주인이 본인임을 확인해요. 이메일로 보낸 메일을 <br />
-            확인해주세요!
-          </Text>
-        </Flex>
-
-        <Flex gap="xs" direction="column" align="flex-start">
-          <Box
-            text={
-              <Flex justify="flex-start" gap="xs">
-                <Text typo="body1" color="sub">
-                  이메일
-                </Text>
-                <Text typo="body1">{maskedEmail}</Text>
-              </Flex>
+      <Flex
+        direction="column"
+        justify="space-between"
+        css={css`
+          flex: 1;
+          ${media.pc} {
+            justify-content: center;
+            gap: 100px;
+          }
+        `}>
+        <Flex
+          gap="xl"
+          direction="column"
+          css={css`
+            ${media.pc} {
+              gap: 100px;
             }
-            status="success"
-          />
+          `}>
+          <Flex direction="column" align="flex-start" gap="sm">
+            <EmailVerificationTitle>본인 인증하기</EmailVerificationTitle>
+            <Text
+              typo="body1"
+              css={css`
+                ${media.pc} {
+                  width: 100%;
+                  text-align: center;
+                }
+              `}>
+              기존에 입력하신 본인 이메일을 통해 현재 계정의 <MobileBreak />
+              주인이 본인임을 확인해요. <br />
+              이메일로 보낸 메일을 <MobileBreak />
+              확인해주세요!
+            </Text>
+          </Flex>
 
-          <GuideList>
-            <li>메일 전송이 최대 30분 가량 늦어질 수 있어요.</li>
-            <li>
-              메일이 보이지 않는 경우 스팸 메일함을 확인해주시고, 스팸
-              메일함에도 없을 경우 카카오톡 채널로 문의해주세요.
-            </li>
-            <li>
-              만약 이메일 수신 이후에 인증 버튼을 눌렀음에도 제대로 인증이 되지
-              않는 경우, 해당 브라우저에서 다시 가입 절차를 진행해주세요.
-            </li>
-          </GuideList>
+          <Flex
+            gap="xs"
+            direction="column"
+            align="flex-start"
+            css={css`
+              ${media.pc} {
+                align-items: center;
+              }
+            `}>
+            <Box
+              text={
+                <Flex justify="flex-start" gap="xs">
+                  <Text typo="body1" color="sub">
+                    이메일
+                  </Text>
+                  <Text typo="body1">{maskedEmail}</Text>
+                </Flex>
+              }
+              status="success"
+            />
+
+            <GuideList>
+              <li>메일 전송이 최대 30분 가량 늦어질 수 있어요.</li>
+              <li>
+                메일이 보이지 않는 경우 스팸 메일함을 확인해주시고, 스팸
+                메일함에도 없을 경우 카카오톡 채널로 문의해주세요.
+              </li>
+              <li>
+                만약 이메일 수신 이후에 인증 버튼을 눌렀음에도 제대로 인증이
+                되지 않는 경우, 해당 브라우저에서 다시 가입 절차를 진행해주세요.
+              </li>
+            </GuideList>
+          </Flex>
         </Flex>
-      </Flex>
 
-      <Button onClick={handleResendEmail}>인증메일 다시 받기</Button>
+        <StyledButton onClick={handleResendEmail}>
+          인증메일 다시 받기
+        </StyledButton>
+      </Flex>
     </Container>
   );
 };
 
 const Container = styled(Flex)`
   position: relative;
-  min-height: calc(100vh - var(--header-height, 0px));
+  min-height: 100vh;
   background-color: ${color.backgroundAlternative};
   padding: 40px 16px;
-  ${media.mobile} {
-    width: 100vw;
+  width: 100vw;
+  ${media.pc} {
+    min-height: calc(100vh - var(--header-height, 0px));
+    align-items: center;
+  }
+`;
+
+const EmailVerificationTitle = styled(Text)`
+  ${typography.h1}
+  ${media.pc} {
+    ${typography.display2}
+    width:100%;
+    text-align: center;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  max-width: 100%;
+  ${media.pc} {
+    max-width: 328px;
   }
 `;
