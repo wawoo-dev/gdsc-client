1d5665fd63afd72ab709a819321c17ed32c25673
tablemin03 <vs201466@naver.com>
2026-04-05 14:40:10 +0900

    refactor: 매직넘버 제거 및 사용하지 않는 함수 제거

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,7 +1,6 @@
-export const customMediaQuery = (minWidth: number): string =>
-  `@media (min-width: ${minWidth}px)`;
+const BREAKPOINT = 993;
+
 export const media = {
-  custom: customMediaQuery,
-  pc: customMediaQuery(993),
-  mobile: `@media (max-width : 992px)`
+  pc: `@media (min-width: ${BREAKPOINT}px)`,
+  mobile: `@media (max-width : ${BREAKPOINT - 1}px)`
 };
